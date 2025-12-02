be90ed5a467494c8e5dc2225dadfcaf29b94819b
MadeByZino <jh990517@naver.com>
2025-12-02 12:29:45 +0900

    <feat>: UserRepository 추가

---
+++ be/StockFlow/src/main/java/com/madebyzino/StockFlow/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.madebyzino.StockFlow.repository;
+
+import com.madebyzino.StockFlow.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+}
